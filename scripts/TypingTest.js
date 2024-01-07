d4a8d1433f76f2200409446e8a98b2353486749b
Claire Mather <clairemather03@gmail.com>
2024-01-07 09:50:03 -0500

    I added the method to TypingTest to remove invalid characters on backspace

--- scripts/TypingTest.js
+++ scripts/TypingTest.js
@@ -2,8 +2,8 @@
  * a class that represents the main functions of the typing test
  */
 export class TypingTest {
-  display = document.getElementById("testText");
-  input = document.getElementById("textInput");
+  display = document.getElementById('testText');
+  input = document.getElementById('textInput');
   text;
 
   constructor(newText, timerObj) {
@@ -20,7 +20,7 @@ export class TypingTest {
     this.text = newText;
     this.#displayText();
     this.timer.reset();
-    this.testLength = this.text.split(" ").length;
+    this.testLength = this.text.split(' ').length;
     this.currentWord = 0;
     this.currentLetter = 0;
     this.testStarted = false;
@@ -57,6 +57,28 @@ export class TypingTest {
   }
 
   /**
+   * helper method for updateTest() that removes an invalid character
+   * if the backspace is pressed for an invalid character
+   * 
+   * @param {the NodeList of the test words} wordList 
+   * @param {the NodeList of the letters of the current word} letterList 
+   */
+  #removeInvalidChar(wordList, letterList) {
+    // create an array of the characters of the word minus the removed invalid character
+    let wordStore = new Array();
+    for (let i = 0; i < letterList.length; i += 1) {
+      if (i != this.currentLetter) {
+        wordStore.push(letterList[i]);
+      }
+    }
+    // update the word in the wordlist to the new characters
+    wordList[this.currentWord].innerHTML = ''
+    for (let i = 0; i < wordStore.length; i += 1) {
+      wordList[this.currentWord].appendChild(wordStore[i]);
+    }
+  }
+
+  /**
    * helper method for updateTest() that adds the most recent input character
    * as an invalid character
    * 
@@ -65,14 +87,12 @@ export class TypingTest {
    * @param {the most recent input character} char 
    */
   #addInvalidChar(wordList, letterList, char) {
-    const wordLength = letterList.length;
     let wordBuffer = new Array();
-    for (let i = 0; i < wordLength; i += 1) {
+    for (let i = 0; i < letterList.length; i += 1) {
       wordBuffer.push(letterList[i]);
     }
-    wordList[this.currentWord].innerText = '';
-    console.log(wordLength);
-    for (let i = 0; i < wordLength; i += 1) {
+    wordList[this.currentWord].innerHTML = '';
+    for (let i = 0; i < wordBuffer.length; i += 1) {
       if (i == this.currentLetter) {
         const wrongLetter = document.createElement('span');
         wrongLetter.innerText = char;
@@ -92,18 +112,17 @@ export class TypingTest {
     let wordList = this.display.childNodes;
     let letterList = wordList[this.currentWord].childNodes;
     const char = this.input.value.slice(-1);
-    console.log(this.input.value);
-    console.log(char);
 
     // the backspace key is pressed
     if (this.input.value.length < this.currentLetter) {
+      this.currentLetter -= 1;
       if (letterList[this.currentLetter].className == 'incorrect') {
-        console.log("hii");
+        this.#removeInvalidChar(wordList, letterList);
       } else {
         letterList.className = '';
       }
-      this.currentLetter -= 1;
 
+    // the character input is a space
     } else if (char == ' ') {
       this.currentWord += 1;
       this.currentLetter = 0;
