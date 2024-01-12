17b1fd4a98e0c4e97495d5357b6145e97b17d6d7
Claire Mather <clairemather03@gmail.com>
2024-01-12 09:32:54 -0500

    these changes were left, my bad

--- scripts/ResultsDisplay.js
+++ scripts/ResultsDisplay.js
@@ -19,36 +19,45 @@ export class ResultsDisplay {
 
     render() {
         this.#calculateResults();
+        document.getElementById('timer').innerText = '';
 
         this.displayDiv.innerHTML = '';
+        /*
         const resultsHeader = document.createElement('span');
         resultsHeader.id = 'results';
         resultsHeader.className = 'center-horizontal';
         resultsHeader.innerText = 'Results';
         this.displayDiv.appendChild(resultsHeader);
+        */
 
         const wpmHeader = document.createElement('span');
         wpmHeader.id = 'WPM';
         wpmHeader.innerText = 'WPM';
+        wpmHeader.className = 'results-header';
         this.displayDiv.appendChild(wpmHeader);
         const wpmData = document.createElement('span');
         wpmData.innerText = this.wpm;
+        wpmData.className = 'results-content';
         this.displayDiv.appendChild(wpmData);
 
         const timeHeader = document.createElement('span');
         timeHeader.id = 'time';
         timeHeader.innerText = 'Time';
+        timeHeader.className = 'results-header';
         this.displayDiv.appendChild(timeHeader);
         const timeData = document.createElement('span');
         timeData.innerText = this.totalTime + 's';
+        timeData.className = 'results-content';
         this.displayDiv.appendChild(timeData);
 
         const accuracyHeader = document.createElement('span');
         accuracyHeader.id = 'accuracy';
         accuracyHeader.innerText = 'Accuracy';
+        accuracyHeader.className = 'results-header';
         this.displayDiv.appendChild(accuracyHeader);
         const accuracyData = document.createElement('span');
         accuracyData.innerText = this.accuracy + '%';
+        accuracyData.className = 'results-content';
         this.displayDiv.appendChild(accuracyData);
     }
 }
